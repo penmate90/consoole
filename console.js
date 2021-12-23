4a49d1cd5f14f49cfcc572d4caa0d981a653ec7b
penmate90 <84581597+penmate90@users.noreply.github.com>
2021-12-23 17:50:06 +0530

    Add files via upload

---
+++ console.js
@@ -0,0 +1,24 @@
+var first_mesaage="Hello";
+var last_message=" User!";
+
+
+var message= first_mesaage.concat( last_message);
+console.log(message);
+
+var a=9;
+var b=10;
+ 
+var add=a+b;
+console.log(add);
+
+var subtract=a-b;
+ console.log(subtract);
+
+ var multiply= a*b;
+
+ console.log(multiply);
+
+ var division=a/b;
+ console.log(division);
+
+
